493ac2c7f65f6fcd76ee855511a6f0a57448cd0b
feberts <64541491+feberts@users.noreply.github.com>
2022-03-09 14:08:27 +0100

    Bash script for german specific replacements edited

--- book3/german.sh
+++ book3/german.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-# german-specific replacements in tex file, because pandoc messes up some things
+# replacements in tex file
 
 tex_file="$1"
 
+# pandoc seems to have issues with german quotation marks in certain situations:
 sed -i -e 's/?``/?{``}/g' "$tex_file" # closing german quotation mark after '?'
 sed -i -e 's/!``/!{``}/g' "$tex_file" # closing german quotation mark after '!'
