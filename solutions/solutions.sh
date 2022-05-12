4e2085f860097eafada3bd6f2bc1b92c842b1393
feberts <64541491+feberts@users.noreply.github.com>
2022-05-12 15:11:08 +0200

    Tidied up solutions.sh

--- solutions/solutions.sh
+++ solutions/solutions.sh
@@ -1,26 +1,10 @@
 #! /bin/bash
 
-# For yucks make the epub
-# # # # cat epub-metadata.txt *.mkd | grep -v '^%' | python pre-html.py | python verbatim.py | pandoc --default-image-extension=svg --css=epub.css -o x.epub
-
-# make the mobi if it works (add verbose for debugging)
-# if hash kindlegen 2>/dev/null; then
-#     kindlegen x.epub 
-#     echo "mobi generated"
-# else
-#     echo "mobi not generated - please install kindlegen"
-# fi
+rm tmp.* *.tmp *.aux solutions.mkd &>/dev/null
 
 python3 generate_mkd.py
 
-rm tmp.* *.tmp *.aux
-# # # # pandoc A0-preface.mkd -o tmp.prefacex.tex
-# # # # sed < tmp.prefacex.tex 's/section{/section*{/' > tmp.preface.tex
-
 cat solutions.mkd | python verbatim.py | tee tmp.verbatim | pandoc -s -N -f markdown+definition_lists -t latex --toc --default-image-extension=eps -V lang:de-DE -V langbabel:ngerman -V fontsize:10pt -V documentclass:book -V title-meta="Python für alle – Lösungen" -V linestretch:1.0 --template=template.latex -o tmp.tex
-# # # # pandoc [A-Z][A-Z]*.mkd -o tmp.app.tex
-
-# # # # sed < tmp.app.tex -e 's/subsubsection{/xyzzy{/' -e 's/subsection{/plugh{/' -e 's/section{/chapter{/' -e 's/xyzzy{/subsection{/' -e 's/plugh{/section{/'  > tmp.appendix.tex
 
 sed < tmp.tex '/includegraphics/s/jpg/eps/' | sed 's"includegraphics{../photos"includegraphics[height=3.0in]{../photos"' > tmp.sed
 diff tmp.sed tmp.tex
@@ -41,4 +25,4 @@ else
   echo "Output on py4e-de-loesungen.pdf"
 fi
 
-rm tmp.* *.tmp *.aux
+rm tmp.* *.tmp *.aux solutions.mkd &>/dev/null
