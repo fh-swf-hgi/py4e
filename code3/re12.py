1e28d6b69641c36762944c2b1c85801298a010b7
feberts <64541491+feberts@users.noreply.github.com>
2023-11-04 11:34:11 +0100

    Fixed errors in excercices

--- code3/re12.py
+++ code3/re12.py
@@ -1,10 +1,9 @@
-# Finde Zeilen, die mit 'Details: rev=' beginnen, gefolgt von einer
-# Zahl mit einer oder mehreren Ziffern und Dezimalpunkten. Diese
-# Zahl ist dann der Match.
+# Finde Zeilen, die mit 'Details:' beginnen und auf 'rev=' enden
+# gefolgt von einer Zahl. Diese Zahl ist dann der Match.
 import re
 hand = open('mbox-short.txt')
 for line in hand:
     line = line.rstrip()
-    x = re.findall('^Details:.*rev=([0-9.]+)', line)
+    x = re.findall('^Details:.*rev=([0-9]+)', line)
     if len(x) > 0:
         print(x)
